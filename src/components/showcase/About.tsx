b32a9b4ccef88b411c6a61d2e467472c6d766298
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-04-07 16:47:31 -0700

    Update About.tsx

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import me from '../../assets/pictures/workingAtComputer.jpg';
 import meNow from '../../assets/pictures/currentme.jpg';
 import { Link } from 'react-router-dom';
+import VideoAsset from '../general/VideoAsset';
 
 export interface AboutProps {}
 
