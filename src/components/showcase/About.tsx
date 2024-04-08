b747d30bdb55d4bab01d176100e423a4e6307d94
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-04-07 18:16:35 -0700

    c

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useEffect } from 'react';
 import me from '../../assets/pictures/workingAtComputer.jpg';
 import meNow from '../../assets/pictures/currentme.jpg';
